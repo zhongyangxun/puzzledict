c94eefdfef99b976b7a6b97dbde40cc727b432a4
zhongyangxun <zhongyangxun@gmail.com>
2026-06-13 18:08:28 +0800

    build(api): switch production API URLs to joeyzcode domains

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,7 @@
 export const DICT_DEV_URL = 'http://127.0.0.1:8789/lookup';
-export const DICT_PROD_URL = 'https://dict-api.zhongyangxun.workers.dev/lookup';
+export const DICT_PROD_URL = 'https://dict.joeyzcode.com/lookup';
 
 export const TRANSLATE_DEV_URL = 'http://127.0.0.1:8787/translate';
-export const TRANSLATE_PROD_URL =
-  'https://translate-gateway.zhongyangxun.workers.dev/translate';
+export const TRANSLATE_PROD_URL = 'https://translate.joeyzcode.com/translate';
 
 export const DAILY_QUOTA_EXCEEDED_CODE = 'DAILY_QUOTA_EXCEEDED';
